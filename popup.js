35fa66b2ed7a46047e9bd273cba17773f4dddf91
стас <stas@MacBook-Pro-stas.local>
2020-12-18 12:30:26 +0200

    ,

--- popup.js
+++ popup.js
@@ -2,6 +2,6 @@ $('.open-popup').click(function(e) {
 e.preventDefault();
     $('.popup-bg').fadein(600);
 });
-$('.open-popup').click(function){
+$('.close-popup').click(function){
     $('.popup-bg').fadein(600);
 });
