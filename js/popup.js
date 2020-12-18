ee640449e6b584b5d2099bd79bfa05c155dcfacd
стас <stas@MacBook-Pro-stas.local>
2020-12-18 14:09:31 +0200

    ,

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
-$('.open-popup').click(function(e) {
-e.preventDefault();
-    $('.popup-bg').fadein(600);
-});
-$('.close-popup').click(function){
-    $('.popup-bg').fadein(600);
-});
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
