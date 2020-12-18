2ed48c7f88f4ebbfbe92f78662436fcf3fe85272
стас <stas@MacBook-Pro-stas.local>
2020-12-18 14:29:32 +0200

    ,

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
+    document.getElementById("myForm").style.display = "flex";
   }
   
   function closeForm() {
